acbd6c7070a17b4f46f7930c9366067f4a170297
Aryo Putra Purwanto <aryoputrap@gmail.com>
2022-05-20 23:21:10 +0700

    first commit

---
+++ Intermediet/tes.py
@@ -0,0 +1,13 @@
+class apel():
+    def __init__(self):
+        super().__init__()
+        print('apel adalah buah')
+    def apaIni(self):
+        print("apel")
+    def warna(self):
+        print("warna merah")
+
+A = apel()
+A.apaIni()
+A.warna()
+A.__init__();
